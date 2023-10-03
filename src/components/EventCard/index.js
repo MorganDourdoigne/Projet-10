313f985f9a050ee1d3f9bb8c04958a78c9d23e80
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-10-03 14:34:06 +0200

    warning 1 ok

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,5 +1,7 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { getMonth } from "../../helpers/Date";
+
 
 import "./style.scss";
 
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,8 +12,6 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-// Si l'index est inférieur à la longueur du tableau byDateDesc moins 1, incrémente l'index
-      // Sinon, réinitialiser l'index à 0 (retourner à la première diapositive)
       () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
@@ -26,7 +24,7 @@ const Slider = () => {
       {byDateDesc?.map((event, idx) => (
         <>
           <div
-key={event.title}
+            key={event.id}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -44,11 +42,11 @@ key={event.title}
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                key={`${event.id}`}
+                  key={event.id}
                   type="radio"
                   name="radio-button"
-// Coche le "dot" si l'index de l'état est égal à radioIdx (manquait "index")
                   checked={index === radioIdx}
+                  readOnly
                 />
               ))}
             </div>

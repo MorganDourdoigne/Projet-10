d109d76e906b8135e34a4da9ae9ce035737b19e4
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-10-19 15:27:43 +0200

    test evencard warning ok

--- src/components/EventCard/index.test.js
+++ src/components/EventCard/index.test.js
@@ -3,10 +3,15 @@ import EventCard from "./index";
 
 describe("When a event card is created", () => {
   it("an image is display with alt value", () => {
-    render(<EventCard imageSrc="http://src-image" imageAlt="image-alt-text" 
-    title="test event"
-    label="test label"
-    />);
+    render(
+      <EventCard 
+        imageSrc="http://src-image" 
+        imageAlt="image-alt-text" 
+        title="test event"
+        label="test label"
+        periode="AVRIL"
+      />
+    );   
     const imageElement = screen.getByTestId("card-image-testid");
     expect(imageElement).toBeInTheDocument();
     expect(imageElement.alt).toEqual("image-alt-text");
